8e8f10292f153d0ec995e18c22ff9284932be5de
Nguyen Trung Viet Anh <vietanh2732004@gmail.com>
2025-12-05 02:44:19 +0700

    Update database creation script to clarify basms_user creation and privileges

--- basms-be/mysql-init/02-create-databases.sql
+++ basms-be/mysql-init/02-create-databases.sql
@@ -7,7 +7,11 @@ CREATE DATABASE IF NOT EXISTS basms_users CHARACTER SET utf8mb4 COLLATE utf8mb4_
 CREATE DATABASE IF NOT EXISTS basms_contracts CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS basms_shifts CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Note: basms_user is automatically created by MYSQL_USER and MYSQL_PASSWORD env vars
+-- in docker-compose.yml. This happens before these scripts run.
+
 -- Grant privileges to basms_user on all databases
+-- Using '%' allows connections from any host (needed for Docker network)
 GRANT ALL PRIVILEGES ON basms_users.* TO 'basms_user'@'%';
 GRANT ALL PRIVILEGES ON basms_contracts.* TO 'basms_user'@'%';
 GRANT ALL PRIVILEGES ON basms_shifts.* TO 'basms_user'@'%';
